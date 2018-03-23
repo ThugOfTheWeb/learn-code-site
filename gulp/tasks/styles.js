b9fedf44d258e3fd81416e5ea0d7519cd521d073
ThugOfTheWeb <thugoftheweb@gmail.com>
2018-03-23 12:56:35 +0600

    Added responssive image and mobile-first mixins

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -2,10 +2,11 @@ const gulp = require('gulp'),
 prefix = require('autoprefixer'),
 postcss = require('gulp-postcss'),
 nested = require('postcss-nested'),
+mixins = require('postcss-mixins'),
 cssImport = require('postcss-import');
 
 gulp.task('styles', () => gulp.src("./app/assets/styles/styles.css")
-    .pipe(postcss([cssImport, nested, prefix]))
+    .pipe(postcss([cssImport, mixins, nested, prefix]))
     .on('error', function(errorInfo) {
         console.log(errorInfo.toString());
         this.emit('end');
